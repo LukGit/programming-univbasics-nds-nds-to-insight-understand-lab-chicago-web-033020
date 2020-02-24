22fdbeca1b7e1ce4d2e02f1e9b52c8a478b9a31f
Shau Yin Luk <ivan.luk@icloud.com>
2020-02-24 18:06:50 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -14,7 +14,7 @@ def print_first_directors_movie_titles
   counter = 0 
   while counter < arrayM.length do
     title = arrayM[counter][:title]
-    print "#{title}"
+    puts "#{title}"
     counter += 1
   end
 end
